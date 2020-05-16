1efad324da1fea1c0dad76e7c666112bda41e0f6
Aimee <961745931@qq.com>
2020-05-16 17:17:50 +0800

    起点中文

---
+++ 20200516/爬取起点中文网/play.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# _*_ coding: UTF-8 _*_
+"""=================================================
+@Project -> File    : Operate_system_ModeView_structure -> play.py
+@IDE     : PyCharm
+@Author  : Aimee
+@Date    : 2020/5/16 17:17
+@Desc    :
+================================================="""
+
