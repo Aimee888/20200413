1fe56b7dcf39f32db5301c860d363fc52f988938
Aimee888 <hbut1210200508@sina.com>
2020-05-21 12:41:41 +0800

    yuyinmodified

--- 20200520/语音控制/control.py
+++ 20200520/语音控制/control.py
@@ -16,7 +16,7 @@ from pcm_wav_transfer import pcm2wave
 
 command_set = {
     "打开QQ": "E:\software\qq\qq\Bin\QQ.exe",
-    "玫瑰": "./exe/rose.exe",
+    "喜欢": "./exe/rose.exe",
     "关机": "shutdown -s -t 600",
     "取消关机": "shutdown -a",
 }
@@ -39,8 +39,17 @@ def main():
     for command in command_set.keys():
         if command in word:
             execute_command = command_set[command]
+            print(execute_command)
             # 打开终端窗口
-            subprocess.Popen(execute_command, shell=True)
+            p = subprocess.Popen("./exe/rose.exe", stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            try:
+                p.wait(timeout=1000)
+            except Exception as e:
+                print("===== process timeout ======")
+                p.kill()
+                return None
+            output = p.communicate()[0]
+            err = p.communicate()[1]
             break
 
 
